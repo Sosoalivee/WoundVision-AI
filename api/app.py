592ea4be9a0909b150a7619119c0bf2a95937329
Bhuvana K L <bhuvanakl.1si23is014@gmail.com>
2026-01-12 21:25:08 +0530

    Fix Flask host and port for Render deployment

--- api/app.py
+++ api/app.py
@@ -98,8 +98,8 @@ def upload_file():
     return render_template('upload.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
 
 
 
